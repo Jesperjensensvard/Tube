7c626f6eaf64d857c01957405d52341124b4a4ff
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-03-27 10:31:23 +0100

    maybye

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function makeRequest(url, method, formdata, callback) {
+    fetch(url, {
+        method: method,
+        body: formdata
+    }).then((data) => {
+        return data.json()
+    }).then((result) => {
+        callback(result)
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+function getCategory() {
+    var requestDataToPhp = new FormData()
+    requestDataToPhp.append("collectionType", "g")
+
+    makeRequest("/DatabaseApi/reguest.php", "POST", requestDataToPhp, (response) => {showCategory(response)})
+}
+
+
+function showCategory(response) {
+    console.log(response)
+}
+
+
+getCategory();
